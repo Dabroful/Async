8d0ffcf9c08acf4bbbb9a9895c354284c42b9212
Vitaly Kovalev <kovalevvitalii@mail.ru>
2023-02-15 20:24:30 +0300

    Done

--- Program.cs
+++ Program.cs
@@ -8,12 +8,13 @@ namespace Potoki.Streams
     {
         public static void Main(string[] args)
         {
-            using (var sw = new StreamWriter(@"D:\Разработка\C#\Potoki.Streams\test.txt", true, Encoding.UTF8))      //путь, перезаписьб кодировка
+            using (var sw = new StreamWriter(@"D:\Разработка\C#\Potoki.Streams\test.txt", true, Encoding.UTF8))      //путь, перезапись, кодировка
             {
                 sw.Write("Hello, World!");
+                sw.WriteLine("Новая строка");
             }
 
-            using (var sr = new StreamReader(@"D:\Разработка\C#\Potoki.Streams\test.txt"))
+            using (var sr = new StreamReader(@"D:\Разработка\C#\Potoki.Streams\test.txt", Encoding.UTF8))
             {
                 var text = sr.ReadToEnd();                                                                                //читает весь файл и записывает в переменную
                 Console.Write(text);
