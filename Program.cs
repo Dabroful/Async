9fcc28c75050b57c6091f272fc220451698fce40
Vitaly Kovalev <kovalevvitalii@mail.ru>
2023-02-15 20:12:22 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using System;
+using System.Text;
+
+namespace Potoki.Streams
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var sw = new StreamWriter(@"D:\Разработка\C#\Potoki.Streams\test.txt", true, Encoding.UTF8))      //путь, перезаписьб кодировка
+            {
+                sw.Write("Hello, World!");
+            }
+
+            using (var sr = new StreamReader(@"D:\Разработка\C#\Potoki.Streams\test.txt"))
+            {
+                var text = sr.ReadToEnd();                                                                                //читает весь файл и записывает в переменную
+                Console.Write(text);
+            }
+        }
+    }
+}
