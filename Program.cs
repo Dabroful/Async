a7df9a0431de3366ffd1296e94f4943cb0f266df
Vitaly Kovalev <kovalevvitalii@mail.ru>
2023-02-17 21:04:39 +0300

    New commit

--- Program.cs
+++ Program.cs
@@ -8,30 +8,7 @@ namespace Potoki.Streams
     {
         public static void Main(string[] args)
         {
-            using (var sw = new StreamWriter(@"D:\Разработка\C#\Potoki.Streams\test.txt", true, Encoding.UTF8))      //путь, перезапись, кодировка
-            {
-                sw.Write("Hello, World!");
-                sw.WriteLine("Новая строка");
-            }
-
-            using (var sr = new StreamReader(@"D:\Разработка\C#\Potoki.Streams\test.txt", Encoding.UTF8))
-            {
-                while (!sr.EndOfStream)
-                {
-                    Console.WriteLine(sr.ReadLine() + "Конец строки");
-                }
-                
-                var text = sr.ReadToEnd();                                                                                //читает весь файл и записывает в переменную
-                Console.Write(text);
-            }
             
-            Console.WriteLine("Введите количество каллорий");
-            int calorie = Convert.ToInt32(Console.ReadLine());
-
-            using (var cal = new StreamWriter(@"D:\Разработка\C#\Potoki.Streams\test.txt"))
-            {
-                cal.Write($"Пользователь ввел {calorie} калорий");
-            }
         }
     }
 }
