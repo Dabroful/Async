3b6484500b0096b151a7f55570adfeea98cda878
Vitaly Kovalev <kovalevvitalii@mail.ru>
2023-02-19 14:03:49 +0300

    Mnogopotochnost

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using System;
 using System.Text;
+using System.Threading;
 
 namespace Potoki.Streams
 {
@@ -8,7 +9,47 @@ namespace Potoki.Streams
     {
         public static void Main(string[] args)
         {
+            Thread thread1 = new Thread(new ThreadStart(DoWork1));                       //создаем поток для метода DoWork без параметров
+            thread1.Start();                                                             //запускаем этот поток
             
+            Thread thread2 = new Thread(new ParameterizedThreadStart(DoWork2));          //создаем поток для метода DoWork с параметрами
+            thread2.Start(int.MaxValue);
+            
+            int j = 0;                                                                   //цикл, который нагружает систему
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                j++;
+                if (i % 10000 == 0)
+                {
+                    Console.WriteLine("Main");
+                }
+            }
+        }
+
+        static void DoWork1()
+        {
+            int j = 0;
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                j++;
+                if (i % 10000 == 0)
+                {
+                    Console.WriteLine("DoWork1");
+                }
+            }
+        }
+        
+        static void DoWork2(object max)
+        {
+            int j = 0;
+            for (int i = 0; i < (int)max; i++)
+            {
+                j++;
+                if (i % 10000 == 0)
+                {
+                    Console.WriteLine("DoWork2");
+                }
+            }
         }
     }
 }
