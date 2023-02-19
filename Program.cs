25087c99fe1a135adad73d4b8d6f50103cbc2060
Vitaly Kovalev <kovalevvitalii@mail.ru>
2023-02-19 15:22:51 +0300

    Пример асинхронной обертки

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using System;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -28,29 +29,58 @@ namespace Potoki.Streams
             // }
             #endregion
 
-            Console.WriteLine("Begin main");
-            DoWorkAsync();                                                                    //вызов асинхронного метода
-            Console.WriteLine("Continue main");
-            
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine("Main");
-            }
-            Console.WriteLine("End main");
+            #region Асинхронность
+            // Console.WriteLine("Begin main");
+            // DoWorkAsync(1000);                                                                    //вызов асинхронного метода
+            // Console.WriteLine("Continue main");
+            //
+            // for (int i = 0; i < 10; i++)
+            // {
+            //     Console.WriteLine("Main");
+            // }
+            // Console.WriteLine("End main");
+            #endregion
+
+            var result = SafeFileAsync("D:\\text.txt");                              //засовываем вызов асинхронного метода в переменную
+            var input = Console.ReadLine();                                                  //делаем доступным ввод в консоли
+            Console.WriteLine(result.Result);
             Console.ReadLine();
         }
 
-        static async Task DoWorkAsync()                                                       //асинхронный метод DoWorkAsync, который будет вызыватт асинхронно DoWork1
+        static async Task<bool> SafeFileAsync(string path)                                        //делаем асинхронную обертку для метода SafeFile
+        {
+            var result = await Task<bool>.Run(() => SaveFile(path));
+            return result;
+        }
+        
+        static bool SaveFile(string path)                                                        //сформировали огромную строку и хотим сохранить в её в файл
+        {
+            var rnd = new Random();
+            var text = "";
+            for (int i = 0; i < 50000; i++)
+            {
+                text += rnd.Next();
+            }
+
+            using (var sw = new StreamWriter(path, false, Encoding.UTF8))
+            {
+                sw.WriteLine();
+            }
+
+            return true;
+        }
+
+        static async Task DoWorkAsync(int max)                                                    //асинхронный метод DoWorkAsync, который будет вызыватт асинхронно DoWork1
         {
             Console.WriteLine("Begin async");
-            await Task.Run(() => DoWork1());                                                  //запуск метода
+            await Task.Run(() => DoWork1(max));                                                  //запуск метода
             Console.WriteLine("End async");
         }
         
-        static void DoWork1()
+        static void DoWork1(int max)
         {
 
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < max; i++)
             {
                 Console.WriteLine("DoWork1");
             }
